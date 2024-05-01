ad442787db3e7e762ffa359fdc38880ec20ca7f4
Ava_Aurora <ava@novakova.io>
2024-04-30 23:36:48 -0400

    Make reaction consume reagent

--- Content.Server/Chemistry/TileReactions/ReplaceTileReaction.cs
+++ Content.Server/Chemistry/TileReactions/ReplaceTileReaction.cs
@@ -12,18 +12,25 @@ namespace Content.Server.Chemistry.TileReactions;
 
 [UsedImplicitly]
 [DataDefinition]
-public sealed partial class ReplaceTileReaction
+public sealed partial class ReplaceTileReaction: ITileReaction
 {
 
     [DataField(required: true)]
     public ProtoId<ContentTileDefinition> Floor { get; set; } = default!;
+
+    [DataField]
+    public FixedPoint2 Usage = FixedPoint2.New(5);
 
     public FixedPoint2 TileReact(TileRef tile, ReagentPrototype reagent, FixedPoint2 reactVolume)
     {
         TileSystem tilesys = IoCManager.Resolve<IEntityManager>().System<TileSystem>();
         ITileDefinitionManager tiledef = IoCManager.Resolve<ITileDefinitionManager>();
         var replacementTile = (ContentTileDefinition) tiledef[Floor];
+        if (reactVolume < Usage) // TODO: Make sure that reagent effect can't turn a tile into the same exact kind of tile to avoid undesired effect stacking
+        {
+            return FixedPoint2.Zero;
+        }
         tilesys.ReplaceTile(tile, replacementTile);
-        return reactVolume;
+        return Usage;
     }
 }
